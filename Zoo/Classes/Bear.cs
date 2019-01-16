e2bfe3c1af3d109c9342763fdac3a0916da9c909
Charles Clemens <cclemensjr@gmail.com>
2019-01-15 18:00:48 -0800

    Implement abstract properties and methods in Bear

--- Zoo/Classes/Bear.cs
+++ Zoo/Classes/Bear.cs
@@ -6,5 +6,21 @@ namespace Zoo.Classes
 {
     class Bear : WarmBlooded
     {
+        public override string Hide { get => Hide; set => Hide = "fur hide"; }
+        public override string Coloring { get => Coloring; set => Coloring = "brown"; }
+        public override bool HasClaws { get => HasClaws; set => HasClaws = true; }
+        public override bool HasWings { get => HasWings; set => HasWings = false; }
+        public override int NumberOfEyes { get => NumberOfEyes; set => NumberOfEyes = 2; }
+        public override int NumberOfLegs { get =>  NumberOfLegs; set => NumberOfLegs = 4; }
+
+        public override void Hunt()
+        {
+            Console.WriteLine(" goes fishing");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine(" lumbers thorugh the forest");
+        }
     }
 }
