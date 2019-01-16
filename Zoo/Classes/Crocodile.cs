03c813bc71f041f2ce5f8ea31c5fca6029522840
CClemensJr <cclemensjr@gmail.com>
2019-01-15 21:30:43 -0800

    Updated Velociraptor and Crocodile classes

--- Zoo/Classes/Crocodile.cs
+++ Zoo/Classes/Crocodile.cs
@@ -6,5 +6,24 @@ namespace Zoo.Classes
 {
     class Crocodile : ColdBlooded
     {
+        public override string Hide { get => Hide; set => Hide = "scaly hide"; }
+        public override string Coloring { get => Coloring; set => Coloring = "green"; }
+        public override bool HasClaws { get => HasClaws; set => HasClaws = true; }
+        public override bool HasWings { get => HasWings; set => HasWings = false; }
+        public override bool ShedsSkin { get => ShedsSkin; set => ShedsSkin = false; }
+        public override int NumberOfEyes { get => NumberOfEyes; set => NumberOfEyes = 2; }
+        public override int NumberOfLegs { get => NumberOfLegs; set => NumberOfLegs = 4; }
+
+        public override void Hunt()
+        {
+            Console.WriteLine(" waits patiently for an unsuspecting victim in the water");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine(" swims quietly through the water");
+        }
+
+
     }
 }
--- Zoo/Classes/Velociraptor.cs
+++ Zoo/Classes/Velociraptor.cs
@@ -6,5 +6,22 @@ namespace Zoo.Classes
 {
     class Velociraptor : ColdBlooded
     {
+        public override string Hide { get => Hide; set => Hide = "fur hide"; }
+        public override string Coloring { get => Coloring; set => Coloring = "gold"; }
+        public override bool HasClaws { get => HasClaws; set => HasClaws = true; }
+        public override bool HasWings { get => HasWings; set => HasWings = false; }
+        public override bool WalksOnAllFours { get => WalksOnAllFours; set => WalksOnAllFours = false; }
+        public override int NumberOfEyes { get => NumberOfEyes; set => NumberOfEyes = 2; }
+        public override int NumberOfLegs { get => NumberOfLegs; set => NumberOfLegs = 2; }
+
+        public override void Hunt()
+        {
+            Console.WriteLine(" stalks it's prey");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine(" runs through the tall grass");
+        }
     }
 }
