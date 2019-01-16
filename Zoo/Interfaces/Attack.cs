c7e1363f313dee98f7bb4d9c418f0111c9ee2930
CClemensJr <cclemensjr@gmail.com>
2019-01-15 23:52:01 -0800

    Created interfaces

---
+++ Zoo/Interfaces/Attack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    interface Attack
+    {
+        string Bite();
+        string Kick();
+        string Push();
+    }
+}
---
+++ Zoo/Interfaces/Living.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    interface Living
+    {
+        string Breath();
+        string Eat();
+        string Scratch();
+        string Sleep();
+    }
+}
