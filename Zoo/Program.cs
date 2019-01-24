d91c52fa800f4d707357b37a4fdca32319c57f3d
CClemensJr <cclemensjr@gmail.com>
2019-01-23 21:21:26 -0800

    Added method so that animal behaviors would appear without user input

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Zoo.Classes;
+using Zoo.Interfaces;
 
 namespace Zoo
 {
@@ -6,7 +8,55 @@ namespace Zoo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WelcomeToTheZoo();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        private static void WelcomeToTheZoo()
+        {
+            Console.WriteLine("Welcome to my zoo!");
+
+
+
+            Console.Write("We have a Lion that:");
+
+            Lion lion = new Lion();
+
+            Console.WriteLine(lion.Scratch());
+
+
+            Console.Write("We have a Tiger that:");
+
+            Tiger tiger = new Tiger();
+
+            Console.WriteLine(tiger.Hunt());
+
+
+            Console.Write("We have a Bear that:");
+
+            Bear bear = new Bear();
+
+            Console.WriteLine(bear.Sleep());
+
+
+            Console.Write("We have a Crocodile that:");
+
+            Crocodile crocodile = new Crocodile();
+
+            Console.WriteLine(crocodile.Move());
+
+
+            Console.Write("And a Velociraptor that:");
+
+            Velociraptor velociraptor = new Velociraptor();
+
+            Console.WriteLine(velociraptor.Move());
+
+            Console.WriteLine("This is probably the best zoo ever");
+
+
         }
     }
 }
